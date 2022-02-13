506a2a969342dc87d5ed7113a02be1bc67e55d15
jiyeon <jy7828@gmail.com>
2022-02-13 23:38:05 +0900

    axios로 데이터 받아와서 뿌려주기

--- src/App.vue
+++ src/App.vue
@@ -14,21 +14,38 @@
       <v-container>
         <v-row>
           <v-col cols="12" sm="2">
-            <v-sheet rounded="lg" min-height="268">
-              <!--  -->
-            </v-sheet>
+            <v-sheet rounded="lg" min-height="268"></v-sheet>
           </v-col>
 
           <v-col cols="12" sm="8">
-            <v-sheet min-height="70vh" rounded="lg">
-              <!--  -->
-            </v-sheet>
+            <v-row
+              min-height="70vh"
+              rounded="lg"
+              v-for="item in list"
+              :key="item.id"
+              class="d-flex pa-2 my-1"
+              style="border:solid gray thin;"
+            >
+              <img :src="item.image" style="width:80px; height:80px;" />
+              <v-col cols="7" class="d-flex flex-column py-0">
+                <div>{{ item.text }}</div>
+                <div class="d-flex" style="gap:5px">
+                  <v-chip v-for="(tag, index) in item.tags" :key="index" small label>#{{tag}}</v-chip>
+                </div>
+                <div>
+                  <span>🧡{{ item.likes }}</span>
+                  <!-- <span>comment</span> -->
+                </div>
+              </v-col>
+              <v-col cols="3" class="d-flex flex-column justify-end align-end">
+                <span>{{ item.owner.lastName }}, {{ item.owner.firstName }}</span>
+                <!-- <span>{{ item.publishDate }}</span> -->
+              </v-col>
+            </v-row>
           </v-col>
 
           <v-col cols="12" sm="2">
-            <v-sheet rounded="lg" min-height="268">
-              <!--  -->
-            </v-sheet>
+            <v-sheet rounded="lg" min-height="268"></v-sheet>
           </v-col>
         </v-row>
       </v-container>
@@ -39,7 +56,21 @@
 <script>
 export default {
   data: () => ({
-    links: ["Dashboard", "Messages", "Profile", "Updates"]
-  })
+    links: ["Dashboard", "Messages", "Profile", "Updates"],
+    list: []
+  }),
+  created() {
+    this.$axios
+      .get("https://dummyapi.io/data/v1/post", {
+        headers: { "app-id": process.env.VUE_APP_API_KEY }
+      })
+      .then(response => {
+        this.list = response.data.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 };
 </script>
+
