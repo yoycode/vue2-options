0d1f6cea59d0e245ec6c9757962bb053287269cc
jiyeon <jy7828@gmail.com>
2022-02-17 00:02:20 +0900

    사용자별 작성글 보기

--- src/App.vue
+++ src/App.vue
@@ -15,14 +15,16 @@
         <v-row>
           <v-col cols="12" sm="3">
             <v-list>
-              <v-list-item v-for="tag in users" :key="tag.id">
-                <v-list-item-avatar>
-                  <v-img :src="tag.picture"></v-img>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title>{{tag.firstName}}, {{tag.lastName}}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
+              <v-list-item-group>
+                <v-list-item v-for="user in users" :key="user.id" @click="postByUser(user)">
+                  <v-list-item-avatar>
+                    <v-img :src="user.picture"></v-img>
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title>{{user.firstName}}, {{user.lastName}}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
             </v-list>
           </v-col>
           <v-col cols="12" sm="9">
@@ -44,8 +46,9 @@ export default {
   },
   data: () => ({
     links: ["Dashboard", "Messages", "Profile", "Updates"],
+    users: [],
     list: [],
-    users: []
+    selectedUser: ""
   }),
   created() {
     this.$axios
@@ -69,6 +72,20 @@ export default {
           console.error(user_err);
         })
       );
+  },
+  methods: {
+    postByUser(user) {
+      this.$axios
+        .get(`https://dummyapi.io/data/v1/user/${user.id}/post`, {
+          headers: { "app-id": process.env.VUE_APP_API_KEY }
+        })
+        .then(response => {
+          this.list = response.data.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
   }
 };
 </script>
